382e916d67e399caf2053a76e7a1df91a2363357
Andre Eisenbach <andre@eisenbach.com>
2020-07-11 01:37:57 +0000

    Add 32-bit lsb_to_msb function

--- utils/byteorder.h
+++ utils/byteorder.h
@@ -4,9 +4,19 @@
 namespace canio {
 namespace utils {
 
+template<typename T>
+constexpr T lsb_to_msb(T);
+
+template<>
 constexpr uint16_t lsb_to_msb(uint16_t lsb) {
   return (lsb & 0xff) << 8 | lsb >> 8;
 }
+
+template<> 
+constexpr uint32_t lsb_to_msb(uint32_t lsb) {
+  return (lsb & 0xff) << 24 | (lsb & 0xff00) << 8 | (lsb & 0xff0000) >> 8 | lsb >> 24;
+}
+
 
 }  // namespace canio
 }  // namespace utils
